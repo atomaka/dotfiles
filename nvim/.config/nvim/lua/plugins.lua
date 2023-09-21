4b8284063c8ea0a0eab0b8128fff7b01f1681d5c
Andrew Tomaka <atomaka@atomaka.com>
2023-09-21 08:25:25 -0400

    Swap nvim package managers

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,53 +1,34 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({
-      'git', 'clone', '--depth', '1',
-      'https://github.com/wbthomason/packer.nvim',
-      install_path
-    })
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  "nvim-lua/plenary.nvim",
+  "nvim-telescope/telescope.nvim",
+  "famiu/bufdelete.nvim",
+  "gpanders/editorconfig.nvim",
+  "johnfrankmorgan/whitespace.nvim",
+  "kylechui/nvim-surround",
+  "lewis6991/gitsigns.nvim",
+  "nyngwang/NeoZoom.lua", -- TODO: Floating window background color
+  "ruifm/gitlinker.nvim",
+  "tpope/vim-eunuch",
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-  local use = use
-
-  use "wbthomason/packer.nvim"
-  -- TODO: autoupdate (but after plugins separate file
-  -- augroup packer_user_config
-  --   autocmd!
-  --   autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  -- augroup end
-
-  use "nvim-lua/plenary.nvim"
-  use "nvim-telescope/telescope.nvim"
-
-  use "famiu/bufdelete.nvim"
-  use "gpanders/editorconfig.nvim"
-  use "johnfrankmorgan/whitespace.nvim"
-  use "kylechui/nvim-surround"
-  use "lewis6991/gitsigns.nvim"
-  use "nyngwang/NeoZoom.lua" -- TODO: Floating window background color
-  use "ruifm/gitlinker.nvim"
-  use "tpope/vim-eunuch"
-
-  use "gruvbox-community/gruvbox"
+  "gruvbox-community/gruvbox",
 
   -- languages
-  use "fatih/vim-go"
-  use "hashivim/vim-terraform"
-  use "leafgarland/typescript-vim"
-  use "tpope/vim-markdown"
-  use "tpope/vim-rails"
-  use "vim-ruby/vim-ruby"
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+  "fatih/vim-go",
+  "hashivim/vim-terraform",
+  "leafgarland/typescript-vim",
+  "tpope/vim-markdown",
+  "tpope/vim-rails",
+  "vim-ruby/vim-ruby",
+})

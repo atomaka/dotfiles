45bc8660ff1b2e6168977612e270a22bee3d862e
Andrew Tomaka <atomaka@atomaka.com>
2024-09-21 14:18:05 -0400

    Use lazy differently

--- nvim/.config/nvim/lua/plugins/current.lua
+++ nvim/.config/nvim/lua/plugins/current.lua
@@ -1,16 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
+return {
   "nvim-lua/plenary.nvim",
   "nvim-telescope/telescope.nvim",
   "famiu/bufdelete.nvim",
@@ -34,4 +22,4 @@ require("lazy").setup({
   "tpope/vim-markdown",
   "tpope/vim-rails",
   "vim-ruby/vim-ruby",
-})
+}
